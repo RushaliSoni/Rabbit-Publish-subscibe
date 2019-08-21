cb9f232c4b6458f63370f81dafa20dee973d0ed4
akshansha <akshanshagohil1995@gmail.com>
2019-08-21 17:01:54 +0530

    Add project files.

---
+++ Receive/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "LocalHost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //Here the exchange type is fanout , 
+                channel.ExchangeDeclare(exchange: "log", type: "fanout");
+
+                // when to declare the queue no parameter is pass
+                var queuename = channel.QueueDeclare().QueueName;
+
+                // using exchane bind the queue 
+                channel.QueueBind(queue: queuename,
+                                    exchange: "log",
+                                    routingKey: "");
+                Console.WriteLine("waiting for a logs");
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                     {
+                         var contain = ea.Body;
+                         var message = Encoding.UTF8.GetString(contain);
+                         Console.WriteLine("[x] {0}", message);
+                     };
+                channel.BasicConsume(queue:queuename,
+                                     autoAck:true,
+                                     consumer:consumer
+                    );
+                Console.WriteLine("Press the Enter for exit.");
+                Console.ReadLine();
+                
+            }
+        }
+    }
+}
---
+++ publish-subscribe/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace publish_subscribe
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "Localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channle = connection.CreateModel())
+            {
+
+                channle.ExchangeDeclare(exchange: "log",type:"fanout" );
+                var message = GetMessage(args);
+                var contain = Encoding.UTF8.GetBytes(message.ToString());
+                channle.BasicPublish(exchange:"log",
+                                     routingKey:"",
+                                     basicProperties:null,
+                                     body:contain);
+                Console.WriteLine("[x]sent[0]", message);
+            }
+            Console.WriteLine("Press Enter For Exit");
+            Console.ReadLine();
+
+
+
+        }
+
+        private static object GetMessage(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                return string.Join(" ", args);
+            }
+            return "Hiiii ";
+            
+        }
+    }
+}
